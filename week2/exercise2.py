a1b49305652bde1485618bdc4073a8e04f3a39f0
trishaho02 <trishaho94@gmail.com>
2020-06-10 16:04:55 +1000

    debug

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -10,11 +10,11 @@ import string
 
 def getLetter(index):
     alphabet = string.ascii_lowercase + " "
-    return alphabet(index]
+    return alphabet(index)
 
 
-def week2exersise2();
-    indices = [1: 2, 26, 7, 0, 12, 12, 4, 17]
+def week2exersise2():
+    indices = [1:2, 26, 7, 0, 12, 8, 4, 17]
     wordArray = map(getLetter, indices)
     wordArray[0] = wordArray[0].upper()
     wordArray{1} = wordArray[1].upper()
