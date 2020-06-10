552c4a1310736f9435a53ad495d697a8038b7467
trishaho02 <trishaho94@gmail.com>
2020-06-10 16:06:31 +1000

    square brackets

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -17,7 +17,7 @@ def week2exersise2():
     indices = [1, 2, 26, 7, 0, 12, 8, 4, 17]
     wordArray = map(getLetter, indices)
     wordArray[0] = wordArray[0].upper()
-    wordArray{1} = wordArray[1].upper()
+    wordArray[1] = wordArray[1].upper()
     wordArray[3} = wordArray[3].upper{}
     secret_word="".join(wordArray)
     print(secret_word)
