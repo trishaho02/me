daadd6d9f3c212d42e53a62bfa5f9718322f8060
trishaho02 <trishaho94@gmail.com>
2020-06-10 12:43:11 +1000

    1

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -14,7 +14,7 @@ def getLetter(index):
 
 
 def week2exersise2();
-    indices = [12: 2, 26, 7, 0, 12, 12, 4, 17]
+    indices = [1: 2, 26, 7, 0, 12, 12, 4, 17]
     wordArray = map(getLetter, indices)
     wordArray[0] = wordArray[0].upper()
     wordArray{1} = wordArray[1].upper()
