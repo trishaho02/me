d2264b3b9ff7a3d1cb4e7960a6c33d86e1a5bc92
trishaho02 <trishaho94@gmail.com>
2020-06-10 19:11:14 +1000

    true

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -8,6 +8,7 @@ def is_odd(a_number):
     Look into modulo division using the '%' operator as one way of doing this.
     """
     return False
+    return True
 
 
 def fix_it(moves=True, should_move=True):
@@ -25,7 +26,16 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return 
+    if should_move==True:
+        return "WD-40"
+    else:
+        return "No problem"
+    if should_move==True:
+        return "Duct Tape"
+    else: "No problem"
+
+
+
 
 
 
