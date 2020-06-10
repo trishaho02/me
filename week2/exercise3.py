a89c977a4112c93823b5703ef6d7b347ed6fc3d7
trishaho02 <trishaho94@gmail.com>
2020-06-10 21:28:35 +1000

    is odd

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,8 +7,12 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    return False
-    return True
+    
+    if a_number%2==1:
+        return True 
+    elif a_number%2==0:
+        return False 
+
 
 
 def fix_it(moves=True, should_move=True):
@@ -46,7 +50,7 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    return 
 
 
 def loops_1c(number_of_items=5, symbol="#"):
