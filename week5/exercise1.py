a155b267fa396b0ca500b217c30bea57e3342541
trishaho02 <trishaho94@gmail.com>
2020-07-22 16:39:13 +1000

    countdown tingz

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -49,7 +49,11 @@ def do_bunch_of_bad_things():
 # return a list of countdown messages, much like in the bad function above.
 # It should say something different in the last message.
 def countdown(message, start, stop, completion_message):
-    pass
+
+    for i in range(9, 0, -1):
+        print("Get ready to start in" + " " + str(i))
+        
+    print("Let's go!")
 
 
 # TRIANGLES
